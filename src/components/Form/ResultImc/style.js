99d5e83cef1c4a8cedd016f028290944720f601c
Diego Fernandes <fernandes.diegod@gmail.com>
2021-12-31 14:29:38 -0300

    Add share button

--- src/components/Form/ResultImc/style.js
+++ src/components/Form/ResultImc/style.js
@@ -19,6 +19,22 @@ const styles = StyleSheet.create({
         fontSize: 18,
         color: "#FF0043",
         fontWeight: "bold"
+    },
+    boxShareButton: {
+        width: "100%",
+        alignItems: "center",
+        marginBottom: 10,
+    },
+    share: {
+        backgroundColor: "#1877f2",
+        borderRadius: 50,
+        paddingBottom: 5,
+        paddingTop: 5
+    },
+    shareText: {
+        color: "#fff",
+        fontWeight: "bold",
+        paddingHorizontal: 30
     }
 })
 
