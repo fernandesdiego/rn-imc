ba73e5dcb088a5abb64a712518c822f96e834499
Diego Fernandes <fernandes.diegod@gmail.com>
2021-12-31 13:36:02 -0300

    Main functionality created

---
+++ src/components/Form/ResultImc/style.js
@@ -0,0 +1,26 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    resultImc: {
+        flex: 1,
+        marginTop: 15,
+        paddingTop: 15,
+        borderRadius: 60,
+        alignItems: "center",
+    },
+    numberImc: {
+        width: "100%",
+        fontSize: 48,
+        color: "#FF0043",
+        fontWeight: "bold"
+    }
+    ,
+    information: {
+        fontSize: 18,
+        color: "#FF0043",
+        fontWeight: "bold"
+    }
+})
+
+
+export default styles
